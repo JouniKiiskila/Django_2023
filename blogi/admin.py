328f6c5c2326bad942761319450dcd8df49b5b1a
JouniKiiskila <77729162+JouniKiiskila@users.noreply.github.com>
2023-02-06 15:00:00 +0200

    06022023_2

--- blogi/admin.py
+++ blogi/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Postaus
 
+
 @admin.register(Postaus)
 class PostausAdmin(admin.ModelAdmin):
-    list_display =["otsikko", "luotu"]
+    list_display = ["otsikko", "luotu"]
