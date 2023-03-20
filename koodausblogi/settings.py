f2f612a97c6dd7388bebfafdeb4cb082ce1c77ac
JouniKiiskila <77729162+JouniKiiskila@users.noreply.github.com>
2023-03-20 15:29:22 +0200

    20032023

--- koodausblogi/settings.py
+++ koodausblogi/settings.py
@@ -15,6 +15,19 @@ from pathlib import Path
 
 import dj_database_url
 
+SENTRY_DSN = os.environ.get("SENTRY_DSN")
+
+if SENTRY_DSN:
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
+        send_default_pii=True
+    )
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,6 +141,20 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Käytä DropBoxia tiedostojen tallentamiseen jos DROPBOX_APP_KEY on asetettu
+DROPBOX_APP_KEY = os.environ.get('DROPBOX_APP_KEY')
+if DROPBOX_APP_KEY:
+    # Loput DropBox-muuttujat haetaan os.environ:sta hakasuluilla [],
+    # jotta niiden puuttuminen aiheuttaisi virheen hyvin aikaisessa
+    # vaiheessa.  Jos käytettäisiin .get():iä, niin virhettä ei tulisi
+    # vielä tässä, mutta vasta silloin, kun DropBoxia yritettäisiin
+    # käyttää.
+    DROPBOX_APP_SECRET = os.environ['DROPBOX_APP_SECRET']
+    DROPBOX_OAUTH2_TOKEN = os.environ['DROPBOX_OAUTH2_TOKEN']
+    DROPBOX_OAUTH2_REFRESH_TOKEN = os.environ['DROPBOX_OAUTH2_REFRESH_TOKEN']
+    # Aseta DropBox Djangon oletus-storageksi
+    DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
