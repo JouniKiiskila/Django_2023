0a52a06ed8e47e8c81978b536e02facc9a69c453
JouniKiiskila <77729162+JouniKiiskila@users.noreply.github.com>
2023-03-20 09:41:43 +0200

    joo

--- create_admin_user.py
+++ create_admin_user.py
@@ -14,10 +14,13 @@ def main():
     email = os.environ.get('ADMIN_USER_EMAIL', '')
 
     users = get_user_model().object
-    
+
     if users.filter(username=username).exist():
         print(f"user {username!r} already exist")
         return
+    
+    if not password:
+        raise SystemExit("ADMIN_USER_PASSWORD needed")
     
     print(f"Creating superuser {username!r}")
     get_user_model().objects.create_superuser(username, email, password)
